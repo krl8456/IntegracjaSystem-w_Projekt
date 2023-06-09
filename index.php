ebd354b5f1d8fac57888515e202207b4cfd95bc2
weronikapotepa <sakita211@zoho.com>
2023-06-09 18:49:38 +0200

    divided data into consumer and non consumer products

--- index.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$mysqli = new mysqli('localhost', 'root', '', 'data');
-if ($mysqli->connect_errno != 0) {
-    echo $mysqli->connect_error;
-}
-
-$json_data = file_get_contents("products.js");
-$products = json_decode($json_data, JSON_OBJECT_AS_ARRAY);
