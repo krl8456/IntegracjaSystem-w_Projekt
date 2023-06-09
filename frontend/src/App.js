15e4d38cfc8ca7de38bca3b4ffc75cdec11d41e9
weronikapotepa <sakita211@zoho.com>
2023-06-09 23:49:33 +0200

    feat: added simple navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,36 @@
-import { Route, Routes } from "react-router";
+import { Route, Routes, Link } from "react-router-dom";
+import { AppBar, Toolbar, Typography, Button } from "@mui/material";
 import Home from "./components/Home";
 import Login from "./components/Login";
 import Register from "./components/Register";
 
 function App() {
   return (
+    <div>
+    <AppBar position="static">
+      <Toolbar sx={{ justifyContent: "space-between" }}>
+        <Typography variant="h6" component="div">
+          My App
+        </Typography>
+        <div>
+          <Button color="inherit" component={Link} to="/">
+            Home
+          </Button>
+          <Button color="inherit" component={Link} to="/login">
+            Login
+          </Button>
+          <Button color="inherit" component={Link} to="/register">
+            Register
+          </Button>
+        </div>
+      </Toolbar>
+    </AppBar>
     <Routes>
       <Route path="/" exact element={<Home />} />
       <Route path="/login" exact element={<Login />} />
       <Route path="/register" exact element={<Register />} />
     </Routes>
+  </div>
   );
 }
 
