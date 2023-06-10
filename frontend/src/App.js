0e4f722cfd0532db2f5d9ceedd7ea51aae505934
krl8456 <karolrak50@gmail.com>
2023-06-10 22:22:38 +0200

    feat: add protected route for dashboard

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,17 +3,28 @@ import Home from "./components/Home";
 import Login from "./components/Login";
 import Register from "./components/Register";
 import Navbar from "./components/Navbar";
-import { UserProvider } from './contexts/UserContext';
+import { UserProvider } from "./contexts/UserContext";
+import RequireAuth from "./components/RequireAuth";
+import Dashboard from "./components/Dashboard";
 function App() {
   return (
     <UserProvider>
-    <Navbar />
-    <Routes>
-      <Route path="/" exact element={<Home />} />
-      <Route path="/login" exact element={<Login />} />
-      <Route path="/register" exact element={<Register />} />
-    </Routes>
-  </UserProvider>
+      <Navbar />
+      <Routes>
+        <Route path="/" exact element={<Home />} />
+        <Route path="/login" exact element={<Login />} />
+        <Route path="/register" exact element={<Register />} />
+        <Route
+          path="/dashboard"
+          exact
+          element={
+            <RequireAuth>
+              <Dashboard />
+            </RequireAuth>
+          }
+        />
+      </Routes>
+    </UserProvider>
   );
 }
 
