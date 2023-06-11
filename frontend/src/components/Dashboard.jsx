3ea9220c233a57d3fe5de096616b477d61d0157b
krl8456 <karolrak50@gmail.com>
2023-06-11 15:55:16 +0200

    feat: add modal for confirmation of account deletion

--- frontend/src/components/Dashboard.jsx
+++ frontend/src/components/Dashboard.jsx
@@ -1,9 +1,4 @@
-import {
-  Typography,
-  Box,
-  Button,
-  useMediaQuery,
-} from "@mui/material";
+import { Typography, Box, Button, useMediaQuery, Modal } from "@mui/material";
 import React, { useContext } from "react";
 import { UserContext } from "../contexts/UserContext";
 import AccountCircleIcon from "@mui/icons-material/AccountCircle";
@@ -12,12 +7,29 @@ import { Link } from "react-router-dom";
 function Dashboard() {
   const mediaBreakpoint = useMediaQuery("(min-width:900px)");
   const { user } = useContext(UserContext);
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
+  const style = {
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+    width: 400,
+    bgcolor: "background.paper",
+    border: "2px solid #000",
+    boxShadow: 24,
+    p: 4,
+  };
 
   if (!user) {
-    return (
-      <></>
-    );
+    return <></>;
   }
+
+  const handleDelete = async () => {
+    
+  };
 
   return (
     <>
@@ -41,26 +53,69 @@ function Dashboard() {
         >
           Profile
         </Typography>
-        <Box sx={{display: "flex", alignItems: "center", gap: "0.5em"}}>
+        <Box sx={{ display: "flex", alignItems: "center", gap: "0.5em" }}>
           <AccountCircleIcon sx={{ fontSize: "5em" }} />
-          <Typography variant="h6" component="p">{user.name}</Typography>
+          <Typography variant="h5" component="p">
+            {user.name}
+          </Typography>
         </Box>
         <Typography variant="body1" component="p">
           <strong>email: </strong>
           {user.email}
         </Typography>
-        <Link
-          to="/update-profile"
-          style={{ textDecoration: "none", color: "black" }}
-        >
+        <Box sx={{ display: "flex", gap: "2em" }}>
+          <Link
+            to="/update-profile"
+            style={{ textDecoration: "none", color: "black" }}
+          >
+            <Button
+              variant="contained"
+              color="secondary"
+              sx={{ width: "15em", mt: "3em" }}
+              onClick={handleDelete}
+            >
+              Update your profile
+            </Button>
+          </Link>
+
           <Button
             variant="contained"
-            color="secondary"
+            color="error"
             sx={{ width: "15em", mt: "3em" }}
+            onClick={handleOpen}
           >
-            Update your profile
+            Delete your profile
           </Button>
-        </Link>
+          <Modal
+            open={open}
+            onClose={handleClose}
+            aria-labelledby="modal-delete-profile"
+            aria-describedby="by clicking this yes in this modal you can delete your account"
+          >
+            <Box sx={style}>
+              <Typography
+                id="modal-modal-title"
+                variant="h3"
+                component="h2"
+                sx={{ mb: 10 }}
+              >
+                Are you sure?
+              </Typography>
+              <Box sx={{display: "flex", gap: 5}}>
+                <Button
+                  onClick={handleDelete}
+                  color="primary"
+                  variant="contained"
+                >
+                  Yes
+                </Button>
+                <Button onClick={handleClose} color="error" variant="contained">
+                  Cancel
+                </Button>
+              </Box>
+            </Box>
+          </Modal>
+        </Box>
       </Box>
     </>
   );
